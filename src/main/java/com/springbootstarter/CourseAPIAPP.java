942957989f5902153fc4d13c6c80df02813cd63d
manish22d <mr.manish22d@gmail.com>
2019-10-19 12:44:40 +0530

    first commmit

---
+++ src/main/java/com/springbootstarter/CourseAPIAPP.java
@@ -0,0 +1,12 @@
+package com.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAPIAPP {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAPIAPP.class, args);
+	}
+}
